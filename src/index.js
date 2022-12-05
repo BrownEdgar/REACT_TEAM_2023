f2caad908714513f3b734f1ae756ba8a1723441c
TatevGevorgyan000 <gevorgyantatev2000@gmail.com>
2022-12-05 20:02:45 +0400

    fix conflict

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import App from './components/Tatev/Todos/App';
-=======
-import App from './components/Tigran/Homework_Languages/App.jsx';
->>>>>>> 073b7ebfebc35f9d976db37eed2b43648f34a9f8
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
