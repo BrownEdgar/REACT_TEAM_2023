d6934fac9e25e928faa05b47e1042b42c5196159
Tigran Pashayan <tigranpashayan1991@mail.ru>
2022-12-06 23:17:47 +0400

    fix conflict

--- src/components/Tigran/Homework_Languages/App.jsx
+++ src/components/Tigran/Homework_Languages/App.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 import Language from './Language'
 
 
+
 export default function App() {
-const [arr, setArr] = useState(["html", "css", "JavaScript", "React.js","Node.js","Python"])
+const [arr, setArr] = useState(["html", "css", "JavaScript", "React.js", "Node.js","Python"])
 const handelDelete = (language) => {
-  const f = arr.filter(elem => elem === elem.language)
+  const f = arr.filter(elem => elem !== language)
   setArr(f)
 }
 
