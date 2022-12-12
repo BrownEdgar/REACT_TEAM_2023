cc4150e18c1668b412bbbb376dde44822c563ca1
Tigran Pashayan <tigranpashayan1991@mail.ru>
2022-12-12 17:18:46 +0400

    Add my homework

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/Hayk/App'
+import App from './components/Tigran/FORM_2_TODO/App'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
