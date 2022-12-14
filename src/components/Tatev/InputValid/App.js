fd44aa64785952188350262b494e23f8e8805f94
TatevGevorgyan000 <gevorgyantatev2000@gmail.com>
2022-12-14 20:14:52 +0400

    homework_todo_valid

--- src/components/Tatev/InputValid/App.js
+++ src/components/Tatev/InputValid/App.js
@@ -1,21 +1,55 @@
 import React, {useState} from 'react'
 
 function App() {
-    const [val,setVal] = useState('')
+    const [val,setVal] = useState({
+		value: "",
+		errorMessage: "Invalid parameter",
+		isValid: true,
+		validatde: {
+			number: false, //
+			upperCase: true, // +
+			required: true, // +
+            noSpace: true, //
+		}
+	})
+    
+    const validate = (value,option) => {
+        let result = true;
+            if(option.required){
+                result = result && value.trim() !== ""
+            }
 
-    const handeleSubmit = (e) => {
-        e.preventDefault()
-        setVal(e.target.value)
+            if(option.upperCase) {
+                result = result && value.match(/[A-Z]/g)
+            }
+
+
+            // if(option.number) {
+            //     result = result && !value.match(/\d/g)
+            //     console.log(!value.match(/\d/g),'4444');
+            // }
+
+            // if(option.noSpace){
+            //     result = result && value.match(/^ *$/) === null;
+            // }
+
+        return result
     }
 
     const handeleChange = (e) => {
-        console.dir(val)
-        setVal(e.target.value)
+        const isValid = validate(e.target.value, val.validatde)
+        if(isValid){
+            setVal({...val, value: e.target.value, isValid: true})
+        }else{
+            setVal({...val, value: e.target.value, isValid: false})
+
+        }
     }
   return (
     <div>
-        <form onSubmit={handeleSubmit}>
-            <input type='text' onChange={handeleChange}/>
+        <form>
+            <input type='text' onChange={handeleChange} value={val.value}/>
+            {!val.isValid && <p id='error'>{val.errorMessage}</p>}
         </form>
     </div>
   )
