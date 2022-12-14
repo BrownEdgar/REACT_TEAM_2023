40e374b2786f688f4b9fcefbf082e902dd00af41
Tigran Pashayan <tigranpashayan1991@mail.ru>
2022-12-14 18:56:51 +0400

    Add homework form validation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/Tigran/FORM_2_TODO/App'
+import App from './components/Tigran/VALIDATION_FORM/App'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
