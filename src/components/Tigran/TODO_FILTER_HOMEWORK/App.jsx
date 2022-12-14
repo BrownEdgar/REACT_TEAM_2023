fa9789a4d495720e10f7ded7783147255d073dd8
Tigran Pashayan <tigranpashayan1991@mail.ru>
2022-12-14 18:59:56 +0400

    Add homework form validation

--- src/components/Tigran/TODO_FILTER_HOMEWORK/App.jsx
+++ src/components/Tigran/TODO_FILTER_HOMEWORK/App.jsx
@@ -25,17 +25,27 @@ useEffect(() => {
 },[])
 
 const getUserByName = name => {
-    const f = user.filter(elem => elem.name.includes(name))
+    const f = user.filter(elem => elem.includes(name))
     setSpecialuser(f)
 }
 
 useEffect(() => {
     getUserByName(message)
 }, [message])
+<<<<<<< HEAD
+
+
+  return (
+    <div>
+        <FilterName 
+					handleSubmit={handleSubmit} 
+					specialuser={specialuser} getUserByName={getUserByName} />
+=======
     
     return (
     <div>
         <FilterName handleSubmit={handleSubmit} specialuser={specialuser}  />
+>>>>>>> 852cd94acba307e953d5d2a4ac5062b352822fcd
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< HEAD
 import App from './components/Tigran/VALIDATION_FORM/App'
+=======
+import App from './components/Gohar/Homework/HW10_password/InputErr/App';
+>>>>>>> 111eae07906082281ec3b5f51d1a2a10ef95a6a3
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
