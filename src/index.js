03232f203be832e05d4b08f95940537db8841eef
Tigran Pashayan <tigranpashayan1991@mail.ru>
2022-12-14 16:36:10 +0400

    new branch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import App from './components/Tigran/FORM_2_TODO/App'
-=======
-import App from './components/Gohar/Homework/Homework_TODO/App.jsx';
-
->>>>>>> 80dce95392b7b01512a58ffab4a9b95477263abd
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
