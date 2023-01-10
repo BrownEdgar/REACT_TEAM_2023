2e0d431b9469f0f7651ab1d5fb23d77f4d907e5c
Arsen1983 <arsen_10@rambler.ru>
2023-01-10 23:10:30 +0400

    add branch

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { CHANGE_COUNT, CHANGE_NAME, ADD_ELEMENT } from "./redux/actionTypes";
-
-
+import { ADD_ELEMENT, CHANGE_COUNT, CHANGE_NAME } from './redux/actionTypes';
 
 function App() {
-	const count = useSelector(state => state.count);
-	const name = useSelector(state => state.name);
-	const arr = useSelector(state => state.arr);
+	const name = useSelector(state => state.name)
+	const count = useSelector(state => state.count)
+	const arr = useSelector(state => state.arr)
 
 	const dispatch = useDispatch()
 	const handleClick = () => {
 		dispatch({ type: CHANGE_COUNT })
 	}
-
 	const handleClick2 = () => {
 		dispatch({ type: CHANGE_NAME })
 	}
 	const handleClick3 = () => {
 		dispatch({ type: ADD_ELEMENT })
 	}
-
 	return (
 		<div className="App">
-			<h1>count: {count}</h1>
-			<h1>Name: {name}</h1>
-			<h1>arr: {JSON.stringify(arr,null, 1)}</h1>
-			<button onClick={handleClick}>change count</button>
-			<button onClick={handleClick2}> change Name</button>
-			<button onClick={handleClick3}> change Name</button>
+			<h1>Hello: {name}</h1>
+			<h1>Count: {count}</h1>
+			<h1>Arr: {JSON.stringify(arr, null, 1)}</h1>
+			<button onClick={handleClick}>Change count</button>
+			<button onClick={handleClick2}>Change Name</button>
+			<button onClick={handleClick3}>Add element</button>
 		</div>
 	);
 }
